a52de1f76b01d521ed8193149a0688cc32efed55
Francisco Lino <franksuriel_7@hotmail.com>
2024-09-10 23:32:55 -0600

    ADD blogs

---
+++ src/config/site-info.ts
@@ -0,0 +1,4 @@
+export const siteInfo = {
+  title: "Frank's Blog",
+  description: "Blog oficial de Francisco Lino (flinodev)",
+};
---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collection = {
+  blog: blogCollection,
+};
